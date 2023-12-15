c00a3da31c263b87b18d39a7b3288138bac44a5f
amaanlari <amaanlari@outlook.comm>
2023-12-16 01:44:08 +0530

    created service layer classes for Load

---
+++ src/main/java/in/net/liveasy/logisticsapi/services/LoadService.java
@@ -0,0 +1,10 @@
+package in.net.liveasy.logisticsapi.services;
+
+import in.net.liveasy.logisticsapi.models.Load;
+
+import java.util.List;
+
+public interface LoadService {
+
+    public List<Load> getLoads();
+}
---
+++ src/main/java/in/net/liveasy/logisticsapi/services/LoadServiceImpl.java
@@ -0,0 +1,24 @@
+package in.net.liveasy.logisticsapi.services;
+
+import in.net.liveasy.logisticsapi.models.Load;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class LoadServiceImpl implements LoadService{
+
+    List<Load> loads;
+
+    public LoadServiceImpl() {
+        loads = new ArrayList<>();
+        loads.add(new Load(1L, "delhi", "jaiput", "chemicals", "canter", 1, 100, "shipper:1123", LocalDate.of(2023, 3, 5)));
+    }
+
+    @Override
+    public List<Load> getLoads() {
+        return loads;
+    }
+}
