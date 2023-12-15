7ea81574957cd4d0b2b5e5bffc8c30c3253f3512
amaanlari <amaanlari@outlook.comm>
2023-12-16 00:39:42 +0530

    created the logistics-api project

---
+++ src/test/java/in/net/liveasy/logisticsapi/LogisticsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package in.net.liveasy.logisticsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
