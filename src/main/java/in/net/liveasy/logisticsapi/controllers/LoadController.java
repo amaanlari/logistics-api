df94181c321700b267d382c4d22dc0542e72756b
amaanlari <amaanlari@outlook.comm>
2023-12-16 01:43:48 +0530

    created controller class for Load

---
+++ src/main/java/in/net/liveasy/logisticsapi/controllers/LoadController.java
@@ -0,0 +1,20 @@
+package in.net.liveasy.logisticsapi.controllers;
+
+import in.net.liveasy.logisticsapi.models.Load;
+import in.net.liveasy.logisticsapi.services.LoadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class LoadController {
+
+    @Autowired
+    private LoadService loadService;
+    @GetMapping("/load")
+    public List<Load> getLoads() {
+        return this.loadService.getLoads();
+    }
+}
